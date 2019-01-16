d9f99eeff0ff30bbe1f8eb8be845e493cb2b0b0d
Eoniz <nathan.artisien@gmail.com>
2019-01-16 18:09:17 +0100

    fixing path

--- back/index.js
+++ back/index.js
@@ -25,7 +25,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "access-control-allow-headers, access-control-allow-method, access-control-allow-methods, access-control-allow-origin, crossdomain, authorization, origin, x-requested-with, Content-Type");
     res.end();
-    
+
     next();
 })
 
@@ -75,9 +75,9 @@ app.get('/print', async (req, res, next) => {
         await asyncForEach(printings, async (file) => {
             console.log(file);
             
-            const { stdout } = await sh(`lp -d Canon_MP280_series ${__dirname}\\public\\files\\${file.name}`);
+            const { stdout } = await sh(`lp -d Canon_MP280_series ${__dirname}/public/files/${file.name}`);
             console.log(stdout);
-            fs.unlinkSync(`${__dirname}\\public\\files\\${file.name}`);
+            fs.unlinkSync(`${__dirname}/public/files/${file.name}`);
         });
     }
     
